c1dccd5a17253a9231662f76af44418fa87b21cf
Georgi Pastrakov <georgi.m.pastrakov.2023@elsys-bg.org>
2026-05-10 11:30:53 +0300

    feat: expose phase 3 public headers

--- include/neuroforge/neuroforge.hpp
+++ include/neuroforge/neuroforge.hpp
@@ -3,6 +3,17 @@
 #include "neuroforge/core/Random.hpp"
 #include "neuroforge/core/Shape.hpp"
 #include "neuroforge/core/Tensor.hpp"
+#include "neuroforge/losses/LossFunction.hpp"
+#include "neuroforge/losses/MSELoss.hpp"
+#include "neuroforge/nn/Linear.hpp"
+#include "neuroforge/nn/Module.hpp"
+#include "neuroforge/nn/Parameter.hpp"
+#include "neuroforge/nn/ReLU.hpp"
+#include "neuroforge/nn/Sequential.hpp"
+#include "neuroforge/nn/Sigmoid.hpp"
+#include "neuroforge/nn/Tanh.hpp"
+#include "neuroforge/optim/Optimizer.hpp"
+#include "neuroforge/optim/SGD.hpp"
 
 namespace neuroforge {
 }
